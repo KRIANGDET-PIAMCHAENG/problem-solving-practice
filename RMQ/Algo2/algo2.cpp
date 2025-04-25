4030a3826ac0f55f68ba9463739bc31fe03832ab
king Kriangdet <kriangdetpiamchaeng@gmail.com>
2025-04-25 13:48:04 +0700

    algo2

--- RMQ/Algo2/algo2.cpp
+++ RMQ/Algo2/algo2.cpp
@@ -12,6 +12,25 @@ int main(){
     for(int i = 0; i<n ; i++)
         cin >> setOfNum[i];
     
+    int minTable[n][n];
+
+    for(int i = 0; i<n ; i++){
+        minTable[i][i] = setOfNum[i];
+        for(int j = i+1; j<n ; j++){
+            minTable[i][j] = min(minTable[i][j-1], setOfNum[j]);
+        }
+    }
+
+    for(int i = 0; i<q ; i++){
+        int l,r;
+        cin >> l >> r;
+        ans[i] = minTable[l][r-1];
+    }
+
+    for(int i = 0; i<q ; i++){
+        cout << ans[i] << endl;
+    }
+
     
 
     return 0;
